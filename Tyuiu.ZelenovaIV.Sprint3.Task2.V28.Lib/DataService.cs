b3c2d8aae96d4d2dead6b4379831ff752c6659dc
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 01:13:47 +0500

    Task2

---
+++ Tyuiu.ZelenovaIV.Sprint3.Task2.V28.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ZelenovaIV.Sprint3.Task2.V28.Lib
+{
+    public class DataService : ISprint3Task2V28
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double result = 0;
+            var k = startValue;
+            do
+            {
+                result = (Math.Pow(2, k) / (k + 1)) * Math.Cos(1.8);
+                k++;
+            } while (k < stopValue);
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.ZelenovaIV.Sprint3.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint3.Task2.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
