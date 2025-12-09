34d1367ae5a442079f82f106c4df8c299f49904c
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 04:05:53 +0500

    Task5

---
+++ Tyuiu.ZelenovaIV.Sprint3.Task5.V30.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;  
+
+namespace Tyuiu.ZelenovaIV.Sprint3.Task5.V30.Lib
+{
+    public class DataService : ISprint3Task5V14
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, k;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries += Math.Sin(x) + (2 / k);
+                }
+
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
+
---
+++ Tyuiu.ZelenovaIV.Sprint3.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint3.Task5.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
