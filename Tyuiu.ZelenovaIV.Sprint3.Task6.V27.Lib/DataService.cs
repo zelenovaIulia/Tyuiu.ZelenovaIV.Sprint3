45a2befadbf87df82709363a4e814fd40c5a446e
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 04:34:46 +0500

    Task6

---
+++ Tyuiu.ZelenovaIV.Sprint3.Task6.V27.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ZelenovaIV.Sprint3.Task6.V27.Lib
+{
+    public class DataService : ISprint3Task6V27
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int res = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 1; d < 11; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        res = res + x;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZelenovaIV.Sprint3.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint3.Task6.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
