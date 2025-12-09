11ad7de4710b930cfb4b8e6a0ff8a24577d7d0f1
zelenovaIulia <zelenova_yulya@internet.ru>
2025-12-10 03:40:05 +0500

    Task4

---
+++ Tyuiu.ZelenovaIV.Sprint3.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ZelenovaIV.Sprint3.Task4.V23.Lib
+{
+    public class DataService : ISprint3Task4V23
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            int x;
+            double mult = 1;
+
+            for (x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0) break;
+                mult *= (Math.Cos(x) / x) + 3;
+            }
+            return Math.Round(mult, 3);
+        }
+    }
+
+}
---
+++ Tyuiu.ZelenovaIV.Sprint3.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZelenovaIV.Sprint3.Task4.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
